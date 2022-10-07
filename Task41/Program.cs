f631e5fd28cfcf484427225d8244ad4f72271e22
Димка я <  @mail.ru>
2022-10-08 01:53:52 +0300

    polzovatel vvodit M chisla bolshe 0

---
+++ Task41/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите колличество чисел: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < n; i++)
+{
+  Console.WriteLine("Введите число : ");
+  int x = Convert.ToInt32(Console.ReadLine());
+  if (x > 0)
+  count++;
+}
+Console.WriteLine($"Количество положительных чисел-> {count}");
